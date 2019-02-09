562ee2a7520d8cfa3f41db03def1116b10a74ed2
Bartosz Przybysz <bartosz.d.przybysz@gmail.com>
2019-02-09 17:57:24 -0500

    home page needs fixing

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -2,15 +2,69 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HomeController {
     @FXML
     ToggleGroup actionSelectToggleGroup;
 
-    public void myGroupAction(ActionEvent action)
-    {
-        System.out.println("Toggled: " + actionSelectToggleGroup.getSelectedToggle().getUserData().toString());
+    @FXML
+    ToggleButton revstatButton, titleDateButton, removeIconsButton, toggleAvailabilityButton, setLandingButton,
+            checkDatesButton;
+
+    @FXML
+    Pane introPage, revstatPage, titleDatePage, removeIconsPage, toggleAvailabilityPage, setLandingPage, checkDatesPage;
+
+    @FXML
+    Button submitButton;
+
+    public void handleToggle(ActionEvent actionEvent) {
+        HashMap<ToggleButton, Pane> paneMap = new HashMap<>();
+
+        paneMap.put(revstatButton, revstatPage);
+        paneMap.put(titleDateButton, titleDatePage);
+        paneMap.put(removeIconsButton, removeIconsPage);
+        paneMap.put(toggleAvailabilityButton, toggleAvailabilityPage);
+        paneMap.put(setLandingButton, setLandingPage);
+        paneMap.put(checkDatesButton, checkDatesPage);
+
+        if (actionSelectToggleGroup.getSelectedToggle() == null) {
+            submitButton.setDisable(true);
+            setPaneVisibility(introPage);
+        } else {
+            submitButton.setDisable(false);
+            setPaneVisibility(paneMap.get(actionSelectToggleGroup.getSelectedToggle()));
+        }
+
+
+    }
+
+    private void setPaneVisibility(Pane activePane) {
+        ArrayList<Pane> panes = new ArrayList<Pane>();
+
+        panes.add(introPage);
+        panes.add(revstatPage);
+        panes.add(titleDatePage);
+        panes.add(removeIconsPage);
+        panes.add(toggleAvailabilityPage);
+        panes.add(setLandingPage);
+        panes.add(checkDatesPage);
+
+        panes.remove(activePane);
+
+        for(Pane p : panes) {
+            p.setVisible(false);
+            p.setManaged(false);
+        }
+
+        activePane.setVisible(true);
+        activePane.setManaged(false);
     }
 
     public void handleSubmit(ActionEvent actionEvent) {
